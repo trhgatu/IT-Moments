596453a6fc1003f704bef794f91623b99a2f43d7
Your Name <you@example.com>
2024-09-26 14:26:04 +0700

    first commit

---
+++ client/src/comporments/Posts.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from 'react'
+import PostItem from './PostItem'
+import { DUMMY_POSTS } from '../data'
+
+
+
+
+const Posts = () => {
+    const [posts, setPosts] = useState(DUMMY_POSTS)
+    return (
+        <section className="posts">
+            {posts.length > 0 ? <div className="container post__container">
+                {
+                    posts.map(({ id, thumbnail, category, title, desc, authorID }) => <PostItem
+                        key={id} postID={id} thumbnail={thumbnail} category={category}
+                        title={title} desc={desc} authorID={authorID} />)
+                }
+            </div> : <h2 className='center'>No post founds</h2>}
+        </section>
+    )
+}
+
+export default Posts
